87be039156ebe363fbb00aec9ced8e54897ea25c
Abdullah Al Tanzim <77851803+Tanzim34@users.noreply.github.com>
2024-09-26 13:09:53 +0600

    Add files via upload

---
+++ CP codes/Number Theory/baby_giant_step.cpp
@@ -0,0 +1,24 @@
+// solve a^x = b mod(m)   - O(sq(m))
+int baby_giant_step(int a, int b, int m) {
+    a %= m, b %= m;
+    int n = sqrt(m) + 1;
+
+    int an = 1;
+    for (int i = 0; i < n; ++i)
+        an = (an * 1ll * a) % m;
+
+    unordered_map<int, int> vals;
+    for (int q = 0, cur = b; q <= n; ++q) {
+        vals[cur] = q;
+        cur = (cur * 1ll * a) % m;
+    }
+
+    for (int p = 1, cur = 1; p <= n; ++p) {
+        cur = (cur * 1ll * an) % m;
+        if (vals.count(cur)) {
+            int ans = n * p - vals[cur];
+            return ans;
+        }
+    }
+    return -1;
+}
